fca4f391ef0dff9dbb029c0b343069a1236e9a83
nalin1304 <76690140+nalin1304@users.noreply.github.com>
2023-12-12 14:16:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, request, session, redirect
 import pymongo
+from bson import ObjectId
 
 app = Flask(" ",template_folder=r"template", static_folder=r"static")
 client = pymongo.MongoClient("mongodb+srv://HealthTrack:HealthTrack@cluster0.azdhcau.mongodb.net/?retryWrites=true&w=majority")
 db = client["HealthTrack"]
 student_data = db["student_data"]
+user_data = db["user_data"]
 #session secret key
 app.config["SECRET_KEY"]="1234567890"
 
+def check_auth():
+    if session.get("user") == None:
+        return False
+    user_id= session.get("id")
+    user_data = user_data.find_one({"_id": ObjectId(user_id)})
+    if user_data == None:
+        return False
+    if user_data["user"] != session.get("user"):
+        return False
+    return True
 
 @app.route("/dashboard")
 def dashboard():
@@ -15,10 +27,13 @@ def dashboard():
         return redirect("/auth")
     class_value = request.args.get("class")
     age_value = request.args.get("age")
+    search_value = request.args.get("search")
     if class_value:
         students = student_data.find({"class": class_value})
     elif age_value:
         students = student_data.find({"age": age_value})
+    elif search_value:
+        students = student_data.find({"name": {"$regex": search_value, "$options": "i"}}).sort("name")
     else:
         students = student_data.find()
     return render_template("main.html", students=list(students))
@@ -27,6 +42,8 @@ def dashboard():
 def auth():
     if session.get("user") == "admin":
         return redirect("/dashboard")
+    if check_auth():
+        return redirect("/home")
     if request.method == "POST":
         user = request.form.get("user")
         pwd= request.form.get("pass")
@@ -35,6 +52,37 @@ def auth():
             session["user"] = user
             print(session)
             return redirect("/dashboard")
+        ud = user_data.find_one({"user": user.lower(), "password": pwd})
+        if ud:
+            session["user"] = user
+            session["id"] = str(ud["user_id"])
+            return redirect("/home")
     return render_template("auth.html")
+@app.route("/home")
+def home():
+    if not check_auth:
+        return redirect("/auth")
+    sutdent = student_data.find_one({"_id": ObjectId(session.get("id"))})
+    return render_template("home.html", student=sutdent)
+
+@app.route("/student_data")
+def student_data_():
+    if session.get("user") != "admin":
+        return redirect("/auth")
+    student_id = request.args.get("id")
+    student = student_data.find_one({"_id": ObjectId(student_id)})
+    print(student)
+    return render_template("student_data.html", student=student)
+
+@app.route("/logout")
+def logout():
+    if session.get("user") == None:
+        return redirect("/auth")
+    session.pop("user")
+    try:
+        session.pop("id")
+    except:
+        pass
+    return redirect("/auth")
 
 app.run(host="0.0.0.0", port=8080, debug=True)
--- processes.py
+++ processes.py
@@ -1,33 +1,44 @@
 import pymongo
+import json
 
 # Replace the connection string with your own MongoDB connection string
 client = pymongo.MongoClient("mongodb+srv://HealthTrack:HealthTrack@cluster0.azdhcau.mongodb.net/?retryWrites=true&w=majority")
 
 db = client["HealthTrack"]
 student_data = db["student_data"]
-
+with open ("recommendation.json") as f:
+  r = json.load(f)
 def bmi_calculator():
   students = student_data.find()
   for student in students:
     print(student)
     height = student["height"] # in feets and inches
     weight = student["weight"]
-    height = str(height).split("'")
-    inches = int(height[0])*12 + int(height[1])
-    height = inches * 0.0254
+    height = float(height)
+
     print(height, weight)
-    bmi = float(weight)/float(height)**2
+    bmi = float(weight)/((float(height)/100)**2)
     print(bmi)
     bmi_category = ""
     if bmi < 18.5:
       bmi_category = "Underweight"
+      recommendation = r["underweight"]["recommendation"]
     elif bmi >= 18.5 and bmi <= 24.9:
       bmi_category = "Normal"
+      recommendation = None
     elif bmi >= 25 and bmi <= 29.9:
       bmi_category = "Overweight"
+      recommendation = r["overweight"]["recommendation"]
     elif bmi >= 30:
       bmi_category = "Obese"
-    student_data.update_one({"_id":student["_id"]},{"$set":{"bmi_category":bmi_category, "bmi":bmi}})
+      recommendation = r["obese"]["recommendation"]
+    old_recommendation = student["recommendation"]
+    if old_recommendation == [recommendation]:
+      recommendation = None
+    if recommendation == None:
+        student_data.update_one({"_id":student["_id"]},{"$set":{"bmi_category":bmi_category, "bmi":bmi}})
+    else:
+        student_data.update_one({"_id":student["_id"]},{"$set":{"bmi_category":bmi_category, "bmi":bmi,"recommendation":[recommendation]}}, upsert=True)
     print(bmi_category)
   
 def avg_data_class(standard):
@@ -43,10 +54,7 @@ def avg_data_class(standard):
     height = student["height"] # in feets and inches
     weight = student["weight"]
     bmi = student["bmi"]
-    height = str(height).split("'")
-    inches = int(height[0])*12 + int(height[1])
-    height = inches * 0.0254
-    height_sum += height
+    height_sum += float(height)/100
     weight_sum += float(weight)
     bmi_sum += bmi
     print(height, height_sum)
@@ -67,5 +75,11 @@ def avg_data_class(standard):
     avg_bmi_category = "Obese"
   db["average_data"].update_one({"class":standard},{"$set":{"avg_height":avg_height,"avg_weight":avg_weight,"avg_bmi":avg_bmi, "avg_bmi_category":avg_bmi_category}}, upsert=True)
   return avg_height
+
+
+def remove_all_recommendation():
+  students = student_data.find()
+  for student in students:
+    student_data.update_one({"_id":student["_id"]},{"$set":{"recommendation":[]}})
 
 bmi_calculator()
